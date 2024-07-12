d0f8ebe4eb253afb383914e19d6f2a3ed2997a2c
wedjat11 <jesusalfonsomontiel@gmail.com>
2024-07-12 17:58:04 -0600

    analiticas de google añadidas

--- genio-blog/pages/_document.js
+++ genio-blog/pages/_document.js
@@ -1,34 +1,36 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-         
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-MR7VRMNS');
-              `,
-            }}
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MR7VRMNS');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-MR7VRMNS`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
           />
-        </body>
-      </Html>
-    );
-  }
+        </noscript>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
