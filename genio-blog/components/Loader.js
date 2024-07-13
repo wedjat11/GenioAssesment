0816f1a550389334fea456a283c53d1b64ccf645
wedjat11 <jamontiel@ammper.com>
2024-07-12 19:44:11 -0600

    loader corregido

--- genio-blog/components/Loader.js
+++ genio-blog/components/Loader.js
@@ -1,13 +1,17 @@
+
+
+import React from 'react';
 import { ClipLoader } from 'react-spinners';
 
 const Loader = ({ loading }) => {
   if (!loading) return null;
-  
+
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-75 z-50">
       <ClipLoader size={50} color={"#EF4444"} loading={loading} />
     </div>
   );
 };
 
 export default Loader;
+
